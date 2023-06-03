7731e0474a44ba97d7e6dd9550aa08dad7bcce7f
Lingeswar03 <132247253+Lingeswar03@users.noreply.github.com>
2023-06-03 02:32:04 -0700

    Update MemberApplication.java

--- src/main/java/com/vagdeviitsol/MemberApplication.java
+++ src/main/java/com/vagdeviitsol/MemberApplication.java
@@ -1,1 +1,13 @@
+package com.vagdeviitsol.MemberApplication;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberApplication.class, args);
+	}
+
+}
